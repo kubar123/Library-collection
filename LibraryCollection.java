ffc0f87b9d2afde4e89a5ec6452ac11e2893b560
kubar123 <kubar123@users.noreply.github.com>
2014-11-05 16:56:49 +1100

    Update LibraryCollection.java

--- LibraryCollection.java
+++ LibraryCollection.java
@@ -1,7 +1,4 @@
-/*
- * Creative Commons Attribution-NonCommercial 3.0 Australia license ( CC BY-NC 3.0 AU)
- * http://creativecommons.org/licenses/by-nc/3.0/au/legalcode  * 
- */
+
 package LibraryCollection;
 
 import java.util.ArrayList;
