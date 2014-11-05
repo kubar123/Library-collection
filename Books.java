4af5a06dd2fa856f4ee3a13127d88c393b048aaa
kubar123 <kubar123@users.noreply.github.com>
2014-11-05 16:56:31 +1100

    Update Books.java

--- Books.java
+++ Books.java
@@ -1,7 +1,3 @@
-/*
- * Creative Commons Attribution-NonCommercial 3.0 Australia license ( CC BY-NC 3.0 AU)
- * http://creativecommons.org/licenses/by-nc/3.0/au/legalcode  * 
- */
 
 package LibraryCollection;
 
